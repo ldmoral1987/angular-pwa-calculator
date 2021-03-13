68df3ee1e5801200e1b010fb950155d843001c0f
Luis del Moral Martínez <ldmoral1987@gmail.com>
2021-03-13 13:41:44 +0100

    Creacion del proyecto, calculadora operativa, pero sin soporte PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,81 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  subText = ''; // The text that should appear in the sub-display
+  mainText = ''; // The text that should appear in the main display
+  operand1: number; // The first operand
+  operand2: number; // The second operand
+  operator = ''; // The operator
+  calculationString = '';
+  // This is the string that denotes the operation being performed
+  answered = false;
+  // A flag to check whether the solution has been processed
+  operatorSet = false; // You'll see how this is used soon
   title = 'angular-calculator-pwa';
+
+  // Line 19 - Line 36
+  pressKey(key: string) {
+    if (key === '/' || key === 'x' || key === '-' || key === '+') {
+      const lastKey = this.mainText[this.mainText.length - 1];
+      if (lastKey === '/' || lastKey === 'x' || lastKey === '-' || lastKey === '+')  {
+        this.operatorSet = true;
+      }
+      if ((this.operatorSet) || (this.mainText === '')) {
+        return;
+      }
+      this.operand1 = parseFloat(this.mainText);
+      this.operator = key;
+      this.operatorSet = true;
+   }
+   if (this.mainText.length === 10) {
+     return;
+   }
+   this.mainText += key;
+}
+
+  allClear() {
+    this.mainText = '';
+    this.subText = '';
+    this.operatorSet = false;
+  }
+
+  getAnswer() {
+    this.calculationString = this.mainText;
+    this.operand2 = parseFloat(this.mainText.split(this.operator)[1]);
+    if (this.operator === '/') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 / this.operand2).toString();
+      this.subText = this.calculationString + "=" + this.mainText;
+      if (this.mainText.length > 9) {
+        this.mainText = this.mainText.substr(0, 9);
+      }
+    } else if (this.operator === 'x') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 * this.operand2).toString();
+      this.subText = this.calculationString + "=" + this.mainText;
+      if (this.mainText.length > 9) {
+        this.mainText = 'ERROR';
+        this.subText = 'Range Exceeded';
+      }
+    } else if (this.operator === '-') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 - this.operand2).toString();
+      this.subText = this.calculationString + "=" + this.mainText;
+    } else if (this.operator === '+') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 + this.operand2).toString();
+      this.subText = this.calculationString + "=" + this.mainText;
+      if (this.mainText.length > 9) {
+        this.mainText = 'ERROR';
+        this.subText = 'Range Exceeded';
+      }
+    } else {
+      this.subText = 'ERROR: Invalid Operation';
+    }
+    this.answered = true;
+    this.mainText = "";
+    this.operatorSet = false;
+  }
 }
